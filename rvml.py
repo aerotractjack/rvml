1dcaf3417c6f8a64b0629dabcf69f0822aa9cdac
jack wolf <jackw@aerotractone.com>
2023-03-22 16:46:10 -0700

    training + predictions

--- rvml.py
+++ rvml.py
@@ -8,21 +8,28 @@ from rastervision.pytorch_learner import (
     ObjectDetectionLearnerConfig)
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
 from rastervision.pytorch_learner.object_detection_utils import TorchVisionODAdapter
+from rastervision.core.data import ObjectDetectionLabels
 import time
+import os
 
 class RVMLTrainer:
 
-    def __init__(self, files):
+    def __init__(self, files, bundle):
         self.tr_img = files.get("train_image_uri")
         self.tr_aoi = files.get("train_aoi_uri")
         self.tr_lab = files.get("train_label_uri")
         self.val_img = files.get("val_image_uri")
         self.val_aoi = files.get("val_aoi_uri")
         self.val_lab = files.get("val_label_uri")
+        self.pred_img = files.get("pred_image_uri")
+        self.pred_aoi = files.get("pred_aoi_uri")
+        self.pred_lab = files.get("pred_label_uri")
+        self.bundle = bundle
         self.cc = None
         self.default_class_id = None
         self.trds = None
         self.vds = None
+        self.pds = None
         self.learner = None
 
     def create_class_config(self, names, colors, default=None):
@@ -63,6 +70,22 @@ class RVMLTrainer:
         )
         self.vds = vds
         return vds
+    
+    def create_pred_ds(self, **kw):
+        pds = ObjectDetectionSlidingWindowGeoDataset.from_uris(
+            class_config=self.cc,
+            image_uri=self.pred_img,
+            size=kw.pop("size", 325),
+            stride=kw.pop("stride", 10000),
+            **kw
+        )
+        self.pds = pds
+        return pds
+    
+    def build_model(self):
+        model = fasterrcnn_resnet50_fpn_v2(num_classes=len(self.cc)+1)
+        model = TorchVisionODAdapter(model)
+        return model
 
     def create_learner(self, **kw):
         data_cfg = ObjectDetectionGeoDataConfig(
@@ -80,11 +103,10 @@ class RVMLTrainer:
             solver=solver_cfg,
             **kw.pop("learner_cfg_kw", {}),
         )
-        model = fasterrcnn_resnet50_fpn_v2(num_classes=len(self.cc)+1)
-        model = TorchVisionODAdapter(model)
+        model = self.build_model()
         learner = ObjectDetectionLearner(
             cfg=learner_cfg,
-            output_dir=kw.pop("output", f"./outputs/{time.monotonic():0.0f}-train"),
+            output_dir=kw.pop("output", self.bundle),
             model=model,
             train_ds=self.trds,
             valid_ds=self.vds,
@@ -98,6 +120,28 @@ class RVMLTrainer:
         self.learner.train(**kw.pop("train_kw", {}))
         self.learner.save_model_bundle()
 
+    def predict(self, **kw):
+        model = self.build_model()
+        predictor = ObjectDetectionLearner.from_model_bundle(
+            os.path.join(self.bundle, "model-bundle.zip"),
+            training=False,
+            model=model,
+            output_dir="./test-pred-out"
+        )
+        pred_generator = predictor.predict_dataset(
+            self.pds,
+            raw_out=True,
+            numpy_out=True,
+            progress_bar=True,
+        )
+        pred_labels = ObjectDetectionLabels.from_predictions(
+            self.pds.windows,
+            pred_generator,
+        )
+        pred_labels.save("preds.json", crs_transformer=self.pds.scene.raster_source.crs_transformer, class_config=self.cc)
+        return pred_labels
+        
+
 if __name__ == "__main__":
     files = {
         "train_image_uri": "data/input.tif", 
@@ -106,11 +150,24 @@ if __name__ == "__main__":
         "val_image_uri": "data/input.tif", 
         "val_label_uri": "data/labels.json", 
         "val_aoi_uri": "data/aoi.json", 
+        "pred_image_uri": "data/input.tif", 
     }
+    bundle = f"outputs/{time.monotonic():0.0f}-train"
 
-    ml = RVMLTrainer(files)
+    ml = RVMLTrainer(files, bundle)
+
+    print("creating class config...")
     ml.create_class_config(["DF"], ["red"])
+    print("creating train ds...")
     ml.create_train_ds(max_windows=5)
+    print("creating val ds...")
     ml.create_val_ds()
+    print("creating pred ds...")
+    ml.create_pred_ds()
+    print("creating learner...")
     ml.create_learner()
-    ml.train()
+    print("training...")
+    ml.train(train_kw={"epochs": 1})
+    print("predicting...")
+    labels = ml.predict()
+    print(labels)
