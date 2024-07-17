3dad0315131e147b4a2880d2f2b02be2e25d7b9f
ja <jackw@aerotractone.com>
2024-07-17 15:44:05 -0700

    work

--- batchpredict.py
+++ batchpredict.py
@@ -6,10 +6,44 @@ import cProfile
 import time
 from enforce_grid import enforce_grid
 from pathlib import Path
+import subprocess
+from pathlib import Path
 
-def batch_predict():
+def downsample_tif(input_path, output_path):
+    try:
+        subprocess.run([
+            'gdal_translate',
+            '-outsize', '50%', '50%',
+            input_path,
+            output_path
+        ], check=True)
+        print(f'Successfully downsampled {input_path} to {output_path}')
+    except subprocess.CalledProcessError as e:
+        print(f'Error downsampling {input_path}: {e}')
+
+def generate_output_path(input_path):
+    # Example: generate output path by appending '_downsampled' to the filename
+    directory = Path(input_path).parent
+    file_name = Path(input_path).name
+    new_file_name = file_name.split(".")[0] + "_downsampled.tif"
+    output_filename = os.path.join(directory,new_file_name)
+    return output_filename
+
+def main():
+    input_file = 'tif_path_list.txt'
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        input_path = line.strip()
+        if input_path:
+            output_path = generate_output_path(input_path)
+            downsample_tif(input_path, output_path)
+
+
+def batch_predict(idx):
     #tifpathlist.txt might need to be command line arg later
-    with open('tifpathlist.txt','r') as text:
+    with open(f'tifpathlists/{idx}.txt','r') as text:
         prediction_list = [line.rstrip('\n') for line in text]
 
     bundle_list = [#'/home/aerotract/GoBag/Migrate/dev/PreTrainedModels/DFModels/ThirdYearDF/Resnet18/bundle/model-bundle.zip',
@@ -28,13 +62,15 @@ def batch_predict():
             split_path = tif.split('Data')
             output_directory = os.path.join('/',split_path[0],'Modeling',model_name+'-products'+str(time.time())+".json")
             
+            tif_d = generate_output_path(tif)
+            # downsample_tif(tif, tif_d)
             print(output_directory)
-            print(tif)
+            print(tif, tif_d)
             print(bundle_uri)
 
+            # subprocess.run(['rastervision','predict',bundle_uri,tif_d,output_directory])
             subprocess.run(['rastervision','predict',bundle_uri,tif,output_directory])
             
-            enforce_grid
             enforce_output = os.path.join(Path(output_directory).parent,"enforced_grid.geojson")
             new_enforce_output = os.path.join(Path(output_directory).parent,"enforced_grid2.geojson")
 
@@ -45,9 +81,8 @@ def batch_predict():
                 wait = os.path.exists(output_directory)
 
             enforce_grid(output_directory,enforce_output)
-            enforce_grid(enforce_output,new_enforce_output)
 
      
 if __name__ == "__main__":
-
-    batch_predict()
+    import sys
+    batch_predict(sys.argv[1])
